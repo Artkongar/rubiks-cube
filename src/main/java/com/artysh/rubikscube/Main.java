a08dd7ea866085caf927b699fc7b15db1d6c31bf
Artkongar <Art.kongar@yandex.ru>
2024-02-15 04:30:46 +0300

    Addde db

--- src/main/java/com/artysh/rubikscube/Main.java
+++ src/main/java/com/artysh/rubikscube/Main.java
@@ -3,36 +3,12 @@ package com.artysh.rubikscube;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-
 @SpringBootApplication
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        /*JsonFactory jsonFactory = new JsonFactory();
-        StringWriter writer = new StringWriter(); // Output will be written to StringWriter
-        JsonGenerator jsonGenerator = jsonFactory.createGenerator(writer);
-
-        jsonGenerator.writeStartObject(); // Start writing a JSON object
-        jsonGenerator.writeStringField("name", "John Doe");
-        jsonGenerator.writeNumberField("age", 30);
-        jsonGenerator.writeBooleanField("isStudent", false);
-
-        jsonGenerator.writeFieldName("hobbies");
-        jsonGenerator.writeStartArray(); // Start writing a JSON array
-        jsonGenerator.writeString("reading");
-        jsonGenerator.writeString("painting");
-        jsonGenerator.writeString("coding");
-        jsonGenerator.writeEndArray(); // End the JSON array
-
-        jsonGenerator.writeEndObject(); // End the JSON object
-
-
-
-        jsonGenerator.flush();
-        jsonGenerator.close();
-        String json = writer.toString();
-        System.out.println(json);*/
+    public static void main(String[] args) {
+        //TODO: defects - fix UI and rotate with cube size 4 and more
+        //TODO: story - auth, scrambler, dynamic result check
         SpringApplication.run(Main.class, args);
     }
 
